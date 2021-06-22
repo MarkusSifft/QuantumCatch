97066bc3db09c736b72ef188f8b7d74457664171
Markus Sifft <m.sifft@icloud.com>
2021-06-22 16:11:17 +0200

    Testing LRUcache

--- simulation.py
+++ simulation.py
@@ -70,7 +70,7 @@ def conditional_decorator(dec, condition):
 
 
 # ------ setup caches for a speed up when summing over all permutations -------
-cache = LRUCache(maxsize=int(170))
+cache = LRUCache(maxsize=int(230))
 cache2 = LFUCache(maxsize=int(10e1))
 cache3 = LFUCache(maxsize=int(10e1))
 cache4 = LFUCache(maxsize=int(3e2))

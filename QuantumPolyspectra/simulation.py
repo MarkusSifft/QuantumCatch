65f4c4da38f02e495c55b74af496ac3187f5858c
Markus Sifft <m.sifft@icloud.com>
2023-06-14 13:56:12 +0200

    Finished: max Cachesize to be chosen automatically

--- QuantumPolyspectra/simulation.py
+++ QuantumPolyspectra/simulation.py
@@ -1220,11 +1220,13 @@ class System(Spectrum):
         else:
             self.gpu_0 = 0
 
-        # estimate neccesarry cachesize
-        #for key in cache_dict.keys():
-        #    update_cache_size(key, out, enable_gpu)
-
-
+        # estimate necessary cachesize (TODO: Anteile könnten noch anders gewählt werden)
+        update_cache_size('cache_fourier_g_prim', self.A_prim, enable_gpu)
+        update_cache_size('cache_first_matrix_step', rho, enable_gpu)
+        update_cache_size('cache_second_matrix_step', rho, enable_gpu)
+        update_cache_size('cache_third_matrix_step', rho, enable_gpu)
+        update_cache_size('cache_second_term', rho[0,0], enable_gpu)
+        update_cache_size('cache_third_term', rho[0,0], enable_gpu)
 
         if order == 2:
             if bar:

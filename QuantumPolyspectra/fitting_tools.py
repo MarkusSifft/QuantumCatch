4e6a9354c3248f2fbae74571da9098a52e45446c
Markus Sifft <m.sifft@icloud.com>
2023-07-18 14:21:13 +0200

    First order can now also be fitted

--- QuantumPolyspectra/fitting_tools.py
+++ QuantumPolyspectra/fitting_tools.py
@@ -340,10 +340,11 @@ class FitSystem:
             fit_list[i] = np.real(self.calc_spec(params, i, f_list[i]))
 
         # ---------- S1 ------------
-        print('S1:')
-        print('measurement:', s_list[1][0], '+/-', sigma * err_list[1][0])
-        print('fit:', fit_list[1])
-        print('relative error:', (s_list[1][0] - fit_list[1]) / s_list[1][0])
+        if fit_list[1] is not None:
+            print('S1:')
+            print('measurement:', s_list[1][0], '+/-', sigma * err_list[1][0])
+            print('fit:', fit_list[1])
+            print('relative error:', (s_list[1][0] - fit_list[1]) / s_list[1][0])
 
         # ---------- S2 ------------
         if fit_list[2] is not None:

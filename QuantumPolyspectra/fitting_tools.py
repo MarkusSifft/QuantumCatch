c543dda57bc462e3ccbeca2842d480788bbe2b21
Markus Sifft <m.sifft@icloud.com>
2023-07-19 14:01:30 +0200

    1. Order can now also be saved

--- QuantumPolyspectra/fitting_tools.py
+++ QuantumPolyspectra/fitting_tools.py
@@ -292,7 +292,7 @@ class FitSystem:
         return out, self.measurement_spec, f_list
 
     def save_fit(self, spec, path, f_list, out):
-        fit_list = []
+        fit_list = {1: None, 2: None, 3: None, 4: None}
         for i in range(1, 5):
             fit_list.append(self.calc_spec(out.params, i, f_list[i]))
 

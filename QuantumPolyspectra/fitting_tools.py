3c1f2dd4cf92ec511efa193bda0ea7e91666b9c8
Markus Sifft <m.sifft@icloud.com>
2023-07-19 11:38:50 +0200

    Cut through S3 and S4 is plotted

--- QuantumPolyspectra/fitting_tools.py
+++ QuantumPolyspectra/fitting_tools.py
@@ -117,8 +117,8 @@ class FitSystem:
         for i, order in enumerate(fit_orders):
             # resid.append(((s_list[i] - calc_spec(params, order, f_list[i]))).flatten()/ np.abs(s_list[i]).max())
             resid.append(
-                ((s_list[order] - self.calc_spec(params, order, f_list[order])) * general_weight[i] / err_list[
-                    order]).flatten())
+                np.abs(((s_list[order] - self.calc_spec(params, order, f_list[order])) * general_weight[i] / err_list[
+                    order]).flatten()))
 
         return np.concatenate(resid)
 

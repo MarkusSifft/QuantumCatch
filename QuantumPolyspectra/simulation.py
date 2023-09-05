c773c4c998f36d8bba4b0dd7024179a05533bb09
Markus Sifft <m.sifft@icloud.com>
2023-09-05 13:10:02 +0200

    Takeing care of the zero eigenvalue

--- QuantumPolyspectra/simulation.py
+++ QuantumPolyspectra/simulation.py
@@ -192,11 +192,12 @@ def _fourier_g_prim(nu, eigvecs, eigvals, eigvecs_inv, enable_gpu, zero_ind, gpu
             raise ValueError(f'There are {sum(small_indices)} eigenvalues smaller than 1e-12. '
                              f'The Liouvilian might have multiple steady states.')
 
-        diagonal = 1 / (-eigvals - 1j * nu)
-        diagonal[zero_ind] = 0
+        #diagonal = 1 / (-eigvals - 1j * nu)
+        #diagonal[zero_ind] = 0
 
-        #diagonal = np.zeros_like(eigvals)
-        #diagonal[~small_indices] = 1 / (-eigvals[~small_indices] - 1j * nu)
+        diagonal = np.zeros_like(eigvals)
+        diagonal[~small_indices] = 1 / (-eigvals[~small_indices] - 1j * nu)
+        diagonal[zero_ind] = 0
 
         Fourier_G = eigvecs @ np.diag(diagonal) @ eigvecs_inv
 
